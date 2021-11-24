73ed3f21672b97498c251356062c6490fa22a0b0
Juan Ortiz <notjuanortiz@gmail.com>
2021-11-24 17:58:34 -0500

    Set crop image directory

--- backend/crops/models.py
+++ backend/crops/models.py
@@ -1,12 +1,16 @@
 from django.db import models
-
 # Create your models here.
 
+def crop_image_directory(instance, filename):
+    return 'images/crop_{0}/{1}'.format(instance.crop.name, filename)
+
 class Crop(models.Model):
-    name = models.CharField(max_length=50)
-    image = models.ImageField()
-    
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to=crop_image_directory)
+
     class Meta:
         verbose_name_plural = 'crops'
+        ordering = ['name']
+
     def __str__(self):
         return self.name
