caf6d34b21c8de0a95141a7a235b4caf552bf623
ananaziz96 <ananaziz15@gmail.com>
2021-11-28 09:33:50 -0500

    route to profile page

---
+++ frontend/src/pages/cropProfile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import MainNavbar from '../components/mainNavbar';
+import {
+  Container,
+  Button,
+  Form
+} from 'react-bootstrap';
+
+const CropProfile = () => {
+  return (
+    <div>
+        <MainNavbar></MainNavbar>
+    
+        <Container style={{width: '500px'}}>
+            <div>hi</div>
+        </Container>
+    </div>
+  );
+}
+export default CropProfile;
