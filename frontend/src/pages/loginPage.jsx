618cf7479ab29798838a07aafe144fa7b25948b3
Juan Ortiz <notjuanortiz@gmail.com>
2021-11-29 15:33:59 -0500

    (WIP) Set up authentication on login page

--- frontend/src/pages/loginPage.jsx
+++ frontend/src/pages/loginPage.jsx
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import MainNavbar from '../components/mainNavbar';
 import {
   Container,
   Button,
   Form
 } from 'react-bootstrap';
+import { authenticationService } from "../services/authentication.service";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
   return (
     <div>
         <MainNavbar></MainNavbar>
     
         <Container style={{width: '500px'}}>
-            <Form>
+            <Form onSubmit= {(e) => {
+              e.preventDefault()
+              authenticationService.login(email,password);
+            }}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)}/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
                 </Form.Group>
                 <Button variant="success" type="submit">
                     Submit
