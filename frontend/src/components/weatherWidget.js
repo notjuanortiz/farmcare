40b2aab39179273d4342fca3092023a74fd0a431
ananaziz96 <ananaziz15@gmail.com>
2021-12-02 00:23:24 -0500

    email/test working

--- frontend/src/components/weatherWidget.js
+++ frontend/src/components/weatherWidget.js
@@ -6,14 +6,34 @@ import {
 } from 'react-bootstrap';
 
 const WeatherWidget = () => {
+
+  const temperature = [];
+
+    //fetch weather data from openweathermap api needs auth
+    async function getWeatherReport() {
+      const apiKey = "6e27a113797392f6dee5a23a3d7cc5ef";
+      const cityName = "London";
+      const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${cityName},uk&APPID=${apiKey}`
+      );
+      const myJson = await response.json();
+
+      const temperature = myJson.weather;
+
+      console.log("today's weather: " , temperature);
+    }
+
+    getWeatherReport();
+
     return (
       <div>
           <Card.Body>
                   <Card.Title>
-                    <p>Temperature: 70 C</p>
+                    {/* <p>Temperature: 70 C</p>
                     <p>Humidity: 5km/h</p>
                     <p>Wind: 5km/h</p>
-                    <p>Precipitation: 0%</p>
+                    <p>Precipitation: 0%</p> */}
+                    {/* {{temperature}} */}
                   </Card.Title>
                   <Card.Subtitle className="mb-2 text-muted">Good condition for</Card.Subtitle>
                 </Card.Body>
