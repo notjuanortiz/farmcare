f398b934b33fe054d2ed6d0ad128dbc08d9c1ae6
Juan Ortiz <notjuanortiz@gmail.com>
2021-12-03 14:46:56 -0500

    Add zipcode to user acp

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -13,7 +13,7 @@ class FarmcareUserAdmin(UserAdmin):
                     'zipcode', 'date_joined',)
     list_filter = ('email', 'is_staff', 'is_active', 'zipcode', 'date_joined',)
     fieldsets = (
-        (None, {'fields': ['first_name', 'last_name', 'email', 'password', ]}),
+        (None, {'fields': ['first_name', 'last_name', 'email', 'zipcode', 'password', ]}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
