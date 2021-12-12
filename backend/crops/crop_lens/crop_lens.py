0c8bfc9b1ef648fc80728c443558c4ddbcef9f8a
Juan Ortiz <8147638+notjuanortiz@users.noreply.github.com>
2021-12-12 10:00:42 -0500

    Revert "crop-lens code"
    This reverts commit ccd751bb1ce0f929056bae697faabc97afb87adf.

--- backend/crops/crop_lens/crop_lens.py
+++ backend/crops/crop_lens/crop_lens.py
@@ -1,230 +1,134 @@
-from functools import total_ordering
-from logging import root
 import os
-from os import listdir
-import cv2 as cv
 from PIL.Image import Image
-from keras import preprocessing
-from keras.backend_config import image_data_format
 import numpy as np
-import pickle 
+from numpy.random.mtrand import f 
 import tensorflow as tf 
 import matplotlib.pyplot as plt
 from tensorflow import keras
-from keras.preprocessing import image
-from keras.preprocessing.image import ImageDataGenerator, img_to_array
+from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from tensorflow.keras.models import Sequential, save_model, load_model
-from sklearn.preprocessing import LabelBinarizer
-from tensorflow.keras.layers import BatchNormalization
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
-from numpy.random import seed
-seed(1)
 
-default_image_size = (128,128)
-height = 128
-width = 128
-directory_root = './plant_village'
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    './plant_village/',
+    batch_size = 32,
+    image_size = (128,128),
+    shuffle = True,
+    label_mode = 'categorical',
+    color_mode = 'grayscale', 
+    seed = 123,
+    validation_split = 0.8,
+    subset = 'training'
+)
+test_ds = tf.keras.utils.image_dataset_from_directory(
+    './plant_village/',
+    batch_size = 32,
+    image_size = (128,128),
+    shuffle = True,
+    label_mode = 'categorical',
+    color_mode = 'grayscale', 
+    seed = 123,
+    validation_split = 0.2,
+    subset = 'validation'
+)
 
-def convert_image_to_array(image):
-    try: 
-        # image = plt.imread(image)
-        image = cv.imread(image)
-        if image is not None: 
-            image = cv.resize(image, default_image_size)
-            return img_to_array(image)
-        else:
-            return img_to_array(image)
-    except Exception as e:
-        print("Error converting image")
-        return None 
+print(train_ds)
+print(test_ds)
 
-def extract_features():
-    image_list, label_list = [], []
+train_ds = train_ds.prefetch(buffer_size = 32)
+test_ds  = test_ds.prefetch(buffer_size = 32)
 
-    try:
-        print("[INFO] Loading images inside dataset...")
-        root_dir = listdir(directory_root)
-        for directory in root_dir :
-            # remove .DS_Store from list
-            if directory == ".DS_Store" :
-                root_dir.remove(directory)
-                print("remove")
+data_augmentation = keras.Sequential(
+    [
+        tf.keras.layers.RandomFlip("horizontal"),
+        tf.keras.layers.RandomRotation(0.1),
+    ]
+)
+def make_model(input_shape, num_classes):
+    inputs = keras.Input(shape=input_shape)
+    # Image augmentation block
+    x = data_augmentation(inputs)
 
-        for plant_folder in root_dir :
-            plant_disease_folder_list = listdir(f"{directory_root}/{plant_folder}")
-            print("[INFO] Processing images inside of class {}...".format(plant_folder))
+    # Entry block
+    x = tf.keras.layers.Rescaling(1.0 / 255)(x)
+    x = tf.keras.layers.Conv2D(32, 3, strides=2, padding="same")(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation("relu")(x)
 
-            for image_file in plant_disease_folder_list:
-                image_dir = directory_root + "/" + plant_folder + "/" + image_file
-                new_name = image_file.replace(" ", "_")
-                new_image_dir = directory_root + "/" + plant_folder + "/" + new_name
-                os.replace(image_dir, new_image_dir)
+    x = tf.keras.layers.Conv2D(64, 3, padding="same")(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation("relu")(x)
 
-                if new_image_dir.endswith(".jpg") == True or new_image_dir.endswith(".JPG") == True or new_image_dir.endswith(".png") == True:
-                    image_list.append(convert_image_to_array(new_image_dir))
-                    label_list.append(plant_folder)
+    previous_block_activation = x  
+    # print(model.summary())
 
-        print("[INFO] Image loading completed")  
-    except Exception as e:
-        print(f"Error : {e}")
-
-
-    return label_list
-
-def train_again():
-
-    train_ds = tf.keras.utils.image_dataset_from_directory(
-        './plant_village',
-        batch_size = 32,
-        image_size = (128,128),
-        shuffle = False, 
-        color_mode = 'grayscale', 
-        seed = 123,
-        validation_split = 0.7,
-        subset = 'training'
-    )
-    test_ds = tf.keras.utils.image_dataset_from_directory(
-        './plant_village',
-        batch_size = 32,
-        image_size = (128,128),
-        shuffle = False,
-        color_mode = 'grayscale', 
-        seed = 123,
-        validation_split = 0.3,
-        subset = 'validation'
-    )
-
-    class_len = len(train_ds.class_names)
-
-    # for dataset to be loaded to buffer from memory
-    AUTOTUNE = tf.data.AUTOTUNE
-    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-    test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
-
-    # to prevent overfitting 
-    data_augmentation = keras.Sequential(
-        [
-            tf.keras.layers.RandomFlip("horizontal", input_shape = (height, width, 1)),
-            tf.keras.layers.RandomRotation(0.1),
-            tf.keras.layers.RandomZoom(0.1)
-        ]
-    )
-
-    def make_model(input_shape, num_classes):
-        inputs = keras.Input(shape=input_shape)
-        # Image augmentation block
-        x = data_augmentation(inputs)
-
-        # Entry block
-        x = tf.keras.layers.Rescaling(1.0 / 255)(x)
-        x = tf.keras.layers.Conv2D(32, 3, strides=2, padding="same")(x)
-        x = tf.keras.layers.BatchNormalization()(x)
+    for size in [128, 256, 512, 728]:
         x = tf.keras.layers.Activation("relu")(x)
-
-        x = tf.keras.layers.Conv2D(64, 3, padding="same")(x)
+        x = tf.keras.layers.SeparableConv2D(size, 3, padding="same")(x)
         x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.Activation("relu")(x)
 
+        x = tf.keras.layers.Activation("relu")(x)
+        x = tf.keras.layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+
+        x = tf.keras.layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = tf.keras.layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = tf.keras.layers.add([x, residual])  
         previous_block_activation = x  
         # print(model.summary())
 
-        for size in [128, 256, 512, 728]:
-            x = tf.keras.layers.Activation("relu")(x)
-            x = tf.keras.layers.SeparableConv2D(size, 3, padding="same")(x)
-            x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation("relu")(x)
 
-            x = tf.keras.layers.Activation("relu")(x)
-            x = tf.keras.layers.SeparableConv2D(size, 3, padding="same")(x)
-            x = tf.keras.layers.BatchNormalization()(x)
-
-            x = tf.keras.layers.MaxPooling2D(3, strides=2, padding="same")(x)
-
-            # Project residual
-            residual = tf.keras.layers.Conv2D(size, 1, strides=2, padding="same")(
-                previous_block_activation
-            )
-            x = tf.keras.layers.add([x, residual])  
-            previous_block_activation = x  
-            # print(model.summary())
-
-        x = tf.keras.layers.SeparableConv2D(1024, 3, padding="same")(x)
-        x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.keras.layers.Activation("relu")(x)
-
-        x = tf.keras.layers.GlobalAveragePooling2D()(x)
-        x = tf.keras.layers.Dropout(0.5)(x)
-        outputs = tf.keras.layers.Dense(num_classes, activation="softmax")(x)
-
-        return keras.Model(inputs, outputs)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Dropout(0.5)(x)
+    outputs = tf.keras.layers.Dense(num_classes, activation="softmax")(x)
+    return keras.Model(inputs, outputs)
 
 
-    model = make_model(input_shape=(128,128) + (1,), num_classes = class_len)
-    # keras.utils.plot_model(model, show_shapes=True)
-    model.summary()
+model = make_model(input_shape=(128,128) + (1,), num_classes=16)
+# keras.utils.plot_model(model, show_shapes=True)
 
-    epochs = 10
+epochs = 5
 
-    callbacks = [
-        keras.callbacks.ModelCheckpoint("model.{epoch:02d}-{accuracy:.2f}.h5"),
-    ]
+# callbacks = [
+#     keras.callbacks.ModelCheckpoint("model.{epoch:02d}-{accuracy:.2f}.h5"),
+# ]
+model.compile(
+    optimizer=keras.optimizers.Adam(1e-3),
+    loss="binary_crossentropy",
+    metrics=["accuracy"],
+)
+model.fit(
+    train_ds, epochs=epochs, validation_data=train_ds, batch_size = 32,
+)
 
-    model.compile(
-        optimizer = keras.optimizers.Adam(1e-3),
-        loss = "sparse_categorical_crossentropy",
-        metrics = ["accuracy"],
-    )
+saved_model_file_path = './.saved_model'
+save_model(model, saved_model_file_path)
 
-    model.fit(
-        train_ds, 
-        epochs = epochs, 
-        validation_data = train_ds, 
-        batch_size = 32,
-    )
+# load the model 
+model = load_model(saved_model_file_path, compile = True)
 
-    saved_model_file_path = './saved_model'
-    save_model(model, saved_model_file_path)
+# for user input image 
+img = keras.preprocessing.image.load_img(
+    "./plant_village/Grape___Black_rot/0a06c482-c94a-44d8-a895-be6fe17b8c06___FAM_B.Rot_5019.JPG", color_mode = 'grayscale', target_size=(128,128,1), interpolation = 'nearest'
+)
+img_array = keras.preprocessing.image.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) 
+print(img_array.shape)
+predictions = model.predict(img_array)
+score = predictions[0]
+# print(
+#     "Image is %.2f "
+#     % (100 * (1 - score), 100 * score)
+# )
+print(predictions)
 
-    return model 
-
-
-def crop_lens():
-    label_list = ['Apple__Apple_scab', 'Apple__Black_rot', 'Apple__Cedar_apple_rust', 'Apple__healthy', 'Blueberry__healthy', 'Cherry_(including_sour)__healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___healthy', 'Corn_(maize)___Northern_Leaf_Blight', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___healthy', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Orange___Haunglongbing_(Citrus_greening)']
-
-    # to train model again 
-    # model = train_again()
-    model = load_model('./saved_model')
-    
-    # # there is a checkpoint warning, -> because we are only using the model on testing data so we can ignore the warning 
-    # checkpoint = tf.train.Checkpoint(model)
-    # save_path = checkpoint.save('./tmp/training_checkpoints')    
-    # checkpoint.restore(save_path).assert_consumed()
-    # evaluate restored model 
-    
-    img = keras.preprocessing.image.load_img(
-        "./plant_village/Apple___healthy/1477ac33-4284-4db3-9a91-dbd61e6183e3___RS_HL_8059.JPG", 
-        color_mode = 'grayscale', 
-        target_size = (128, 128, 1), 
-        interpolation = 'nearest'
-    )
-    img_array = keras.preprocessing.image.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0) 
-    # loss, acc = model.evaluate(img_array)
-    # print('Restored model, accuracy: {:5.2f}%'.format(100 * acc))
-
-    print(model.predict(img_array).shape)
-    predictions = model.predict(img_array)
-    score = tf.nn.softmax(predictions[0])
-
-    # can extract label list from extract_features()
-    print(
-        "This image most likely belongs to {} with a {:.2f} percent confidence."
-        .format(label_list[np.argmax(score)], 100 * np.max(score))
-    )
-    return label_list[np.argmax(score)]
-
-# extract_features()
-crop_name = crop_lens()
-print(crop_name)
+classes = np.argmax(predictions, axis = 1)
+print(classes)
