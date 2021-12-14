5f9f32c89ad43907eb72c1f138a1581720671e16
Juan Ortiz <notjuanortiz@gmail.com>
2021-12-14 02:02:30 -0500

    Fix user_crop serialization
    - Also documents crop views

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -3,38 +3,42 @@ from dj_rest_auth.serializers import UserDetailsSerializer
 from dj_rest_auth.registration.serializers import RegisterSerializer
 from users.models import FarmcareUser, UserCrop
 
+
 class FarmcareRegisterSerializer(RegisterSerializer):
-    zipcode = serializers.IntegerField(required = False)
+    zipcode = serializers.IntegerField(required=False)
 
     def get_cleaned_data(self):
         data_dict = super().get_cleaned_data()
         data_dict['zipcode'] = self.validated_data.get('zipcode')
         return data_dict
 
+
 class FarmcareUserDetailsSerializer(UserDetailsSerializer):
     model = FarmcareUser
-    fields = ('pk','email')
+    fields = ('pk', 'email')
     read_only_fields = ('pk', 'email')
 
-class UserSerializer(serializers.ModelSerializer):
+
+class UserCropSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserCrop
+        fields = [
+            'user_submitted_image'
+        ]
+        depth = 1
+
+
+class UserSerializer(serializers.Serializer):
     '''
     Serializes our user model when accessing '/users'
     '''
+    crops = UserCropSerializer(source='crop_set')
+
     class Meta:
         model = FarmcareUser
-        fields = [ # The fields to display
+        fields = [  # The fields to display
             'pk',
             'first_name',
             'crops',
             'zipcode'
-        ]
-
-class UserCropSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(source='crop.name')
-    image_url = serializers.CharField(source='user_submitted_image')
-    class Meta:
-        model = UserCrop
-        fields = [
-            'name',
-            'image_url'
         ]
