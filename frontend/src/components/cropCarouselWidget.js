a2ef30c5289cf3583b9f7222e24cc8f6bfca64fe
ananaziz96 <ananaziz15@gmail.com>
2021-12-14 12:47:11 -0500

    crop on dashboard

--- frontend/src/components/cropCarouselWidget.js
+++ frontend/src/components/cropCarouselWidget.js
@@ -2,36 +2,73 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import farmcare_logo from '../assets/farmcare_logo.PNG';
 import {Link} from "react-router-dom";
 import {
-  Navbar,
-  Container,
-  Offcanvas,
-  Nav,
-  Carousel
+  Row,
+  Col
 } from 'react-bootstrap';
 import { useState } from "react";
 
 const CropCarouselWidget = () => {
   const [crop, setCrop] = useState("");
-  const auth_token = localStorage.getItem("access-token");
-  fetch('http://localhost:8000/dashboard', {
+  const [image, setImage] = useState("");
+  
+  async function getCrop(){
+    const auth_token = localStorage.getItem("access-token");
+    const response = await fetch('http://localhost:8000/dashboard', {
       method: 'GET',
       headers: {
         'Authorization': `Bearer ${auth_token}`
       }
     })
-    .then(response => { 
-      console.log("user info: ", response.user);
-      let myCrops = response.user.crops;
-      setCrop(myCrops);
-      console.log("crop profile: ", response)
-    })
     .catch(err=>{
       console.log(err)
     })
 
+    try {
+      const json = await response.json();
+      const crop = json.user[0].crops[0].name;
+      const imageUrl = json.user[0].crops[0].image_url;
+      setCrop(crop);
+      setImage(imageUrl);
+
+    } catch (error) {
+      console.error("err", error);
+    }
+
+  }
+
+  getCrop();
+
     return (
       <>
-       {crop}
+      <Row>
+        <Col>
+            <div>
+              <Link to="/apple-profile">Apple</Link>
+              <img 
+                      src="https://www.isons.com/wp-content/uploads/2016/10/apples-on-branch.jpg"
+                      width="100px"
+                      height="70px"
+              />
+            </div>
+          </Col>
+            <Col>
+            <div>
+              <Link to="/cron-profile">Corn</Link>
+              <img 
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Zea_mays_-_K%C3%B6hler%E2%80%93s_Medizinal-Pflanzen-283.jpg/330px-Zea_mays_-_K%C3%B6hler%E2%80%93s_Medizinal-Pflanzen-283.jpg"
+                      width="100px"
+                      height="70px"
+              />
+            </div>
+          </Col>
+        <Col>
+          <div>
+            {crop}
+            <img scr={image} />
+          </div>
+        </Col>
+      </Row>
+
       </>
     );
 }
