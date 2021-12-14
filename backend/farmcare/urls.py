ae9b053b738bb22c6bb39d7ff2b7bf800ebcc356
Juan Ortiz <notjuanortiz@gmail.com>
2021-12-14 15:16:23 -0500

    Revert api url changes

--- backend/farmcare/urls.py
+++ backend/farmcare/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import DefaultRouter
@@ -36,9 +36,9 @@ router.register(r'crops', CropViewSet)
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'auth/', include('dj_rest_auth.urls')),
-    url(r'auth/registration/', include('dj_rest_auth.registration.urls')),
+    path(r'^admin/', admin.site.urls),
+    path(r'auth/', include('dj_rest_auth.urls')),
+    path(r'auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('dashboard', UserDashboardView.as_view(), name='dashboard'),
